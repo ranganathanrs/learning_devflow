c52a0943fb88cd38b00cb6f9b40c12f95ba422ba
ranganathanrs <rangaspo@gmail.com>
2025-02-04 13:29:39 +0530

    Tailwind CSS Setup & Theming

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Hello from "./components/hello";
 export default function Home() {
   console.log('What am i doing here? -- SERVRE/Client?');
   return (
-    <div><h1 className="text-3xl">Welcome to the world of Next.js</h1>
+    <div><h1 className="text-3xl text-light-500">Welcome to the world of Next.js</h1>
     <h2>Hello World!</h2>
     </div>
   );
